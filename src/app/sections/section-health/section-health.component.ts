c8216a4d24ee1d2dae154173f90a0c762e3260ae
Femonofsky <femmy4lov@gmail.com>
2019-08-12 23:18:34 +0100

    worked on section health

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Server } from './../../shared/server';
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-section-health',
   templateUrl: './section-health.component.html',
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class SectionHealthComponent implements OnInit {
 
   constructor() { }
+
+  servers: Server[] = [
+    { id: 1, name: 'dev-web', isOnline: true},
+    { id: 2, name: 'prod-web', isOnline: false},
+    { id: 3, name: 'dev-mail', isOnline: true},
+    { id: 4, name: 'prod-web', isOnline: true}
+  ];
+
 
   ngOnInit() {
   }
