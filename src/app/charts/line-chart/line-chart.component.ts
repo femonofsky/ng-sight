3ac6396fd8b1575e36da8f82a4b818ccad875bd7
Femonofsky <femmy4lov@gmail.com>
2019-08-12 21:34:19 +0100

    added line-chart and finish first mockup for sales section and order section

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,5 +1,13 @@
+import { LINE_CHART_COLORS } from './../../shared/chart.colors';
 import { Component, OnInit } from '@angular/core';
 
+ const SAMPLE_LINE_CHART_DATA:any[] = [
+  { data: [ 32, 14, 46, 23, 38, 56], label: 'Sentiment Analysis'},
+  { data: [ 65, 59, 80, 81, 56, 54], label: 'Image Recognition'},
+  { data: [ 12, 34, 49, 53, 68, 62], label: 'Forecasting'}
+];
+
+const SAMPLE_LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
@@ -8,7 +16,16 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+  public lineChartData: any[] = SAMPLE_LINE_CHART_DATA;
+  public lineChartLabels: string[] = SAMPLE_LINE_CHART_LABELS;
+  public lineChartType = 'line';
+  public lineChartLegend = true;
+  public lineChartOptions: any = {
+    // scaleShowVerticalLines: false,
+    responsive: true
+  }
 
+  public lineChartColors = LINE_CHART_COLORS;
   ngOnInit() {
   }
 
--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,5 +1,5 @@
+import { Order } from './../../shared/order';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-section-orders',
   templateUrl: './section-orders.component.html',
@@ -8,6 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    { id: 1,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+      total: 230, placed: new Date(2019, 1, 1),  fulfilled: new Date(2019, 2, 1), status: 'Completed'},
+    { id: 2,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+       total: 230, placed: new Date(2019,1,1),  fulfilled: new Date(2019,2,1), status: 'Completed'},
+    { id: 3,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+      total: 230, placed: new Date(2019,1,1),  fulfilled: new Date(2019,2,1), status: 'Completed'},
+    { id: 4,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+      total: 230, placed: new Date(2019,1,1),  fulfilled: new Date(2019,2,1), status: 'Completed'},
+    { id: 5,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+      total: 230, placed: new Date(2019,1,1),  fulfilled: new Date(2019,2,1), status: 'Completed'},
+    { id: 6,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+      total: 230, placed: new Date(2019,1,1),  fulfilled: new Date(2019,2,1), status: 'Completed'},
+    { id: 7,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+      total: 230, placed: new Date(2019,1,1),  fulfilled: new Date(2019,2,1), status: 'Completed'},
+    { id: 8,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+      total: 230, placed: new Date(2019,1,1),  fulfilled: new Date(2019,2,1), status: 'Completed'},
+    { id: 9,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+      total: 230, placed: new Date(2019,1,1),  fulfilled: new Date(2019,2,1), status: 'Completed'},
+    { id: 10,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+      total: 230, placed: new Date(2019,1,1),  fulfilled: new Date(2019,2,1), status: 'Completed'},
+    { id: 11,
+      customer: { id: 1, name: 'samuel', state: 'CO', email: 'femmy4lov@gmail.com'},
+      total: 230, placed: new Date(2019,1,1),  fulfilled: new Date(2019,2,1), status: 'Completed'},
+  ];
 
   ngOnInit() {
   }
