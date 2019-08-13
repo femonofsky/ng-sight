3c5a8094e9c80e252e1252ffb9ee553725fc28bf
Femonofsky <femmy4lov@gmail.com>
2019-08-13 14:43:35 +0100

    added Api services

---
+++ src/app/pagination/pagination.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss']
+})
+export class PaginationComponent implements OnInit {
+
+  @Input() page: number;
+  @Input() count: number;
+  @Input() perPage: number;
+  @Input() pagesToShow: number;
+  @Input() loading: number;
+
+  @Output() goPrev = new EventEmitter<boolean>();
+  @Output() goNext = new EventEmitter<boolean>();
+  @Output() goPage = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onPrev(): void
+  {
+    this.goPrev.emit(true);
+  }
+
+  onNext(): void
+  {
+    this.goNext.emit(true);
+  }
+
+  onPage(n: number): void
+  {
+    this.goPage.emit(n);
+  }
+
+  totalPages(): number {
+    return Math.ceil(this.count / this.perPage) || 0;
+  }
+
+  isLastPage(): boolean {
+    return this.perPage * this.page >= this.count;
+  }
+
+  getPages(): number[] {
+    const totalPages = Math.ceil(this.count / this.perPage);
+    const thisPage = this.page || 1;
+    const pageToShow = this.pagesToShow || 9;
+    const pages: number[] = [];
+    pages.push(thisPage);
+
+    for (let i = 0; i < pageToShow - 1; i++) {
+      if (pages.length < pageToShow)
+      {
+        if (Math.min.apply(null, pages) > 1) {
+          pages.push(Math.min.apply(null, pages) - 1);
+        }
+      }
+
+      if (pages.length < pageToShow) {
+        if (Math.max.apply(null, pages) > 1) {
+          pages.push(Math.max.apply(null, pages) + 1);
+        }
+      }
+    }
+    pages.sort((a, b) => a - b);
+    return pages;
+
+  }
+
+  getMin(): number {
+    return (( this.perPage * this.page) - this.perPage) + 1;
+  }
+
+  getMax(): number {
+    let max = this.perPage * this.page;
+    if (max > this.count){
+      max = this.count
+    }
+    return max;
+  }
+
+}
---
+++ src/app/services/sale-data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+// import {Http } from '@angular/http';
+import { HttpClient} from '@angular/common/http';
+// import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
+
+
+@Injectable()
+export class SaleDataService {
+  baseUrl = 'http://localhost:5000/api/order/';
+  constructor(private _http: HttpClient) {}
+
+  getOrders(pageIndex: number, pageSize: number) {
+    return this._http.get( this.baseUrl + pageIndex + '/' + pageSize);
+  }
+}
+
