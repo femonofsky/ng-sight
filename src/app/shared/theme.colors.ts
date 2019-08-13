dd4445c803537a6b0614e04f618b6dad8bd5e830
Femonofsky <femmy4lov@gmail.com>
2019-08-13 17:45:12 +0100

    createm theme for the application

---
+++ src/app/shared/theme.colors.ts
@@ -0,0 +1,25 @@
+import { ThemeColors} from './theme.colors.model';
+
+export const THEME_COLORS: ThemeColors[] = [
+  {
+    name: 'Default',
+    colorSet: [
+      '#083d77',
+      '#ff6b6b',
+      '#f4d35e',
+      '#ee964b',
+      '#f9573b'
+    ]
+  },
+  {
+    name: 'Bright',
+    colorSet: [
+      '#26547c',
+      '#fe1eee',
+      '#ffd166',
+      '#06d6a0',
+      '#fcfcfc'
+    ]
+  },
+
+];
